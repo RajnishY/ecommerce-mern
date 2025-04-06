18dac6d94ece5f66f8f6f851cfaaf38cbbbed61d
Rajnish <rajnishy5398@gmail.com>
2025-04-06 15:39:40 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/ecommerce";
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
-
-app.get("/", (req, res) => res.send("API is running"));
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use("/api/users", userRoutes);
+app.listen(process.env.PORT, () => console.log(`🚀 Server running on port ${process.env.PORT}`));
